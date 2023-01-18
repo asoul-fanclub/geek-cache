e75e00de9986bae81d977ba1fc5fe146af27bbd4
Makonike <1694585033@qq.com>
2023-01-18 13:37:28 +0800

    test: add test for lru

--- lru/lru_test.go
+++ lru/lru_test.go
@@ -1,6 +1,7 @@
 package lru
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -46,5 +47,37 @@ func TestCache_Add(t *testing.T) {
 	lru.Add("key", String("122"))
 	if lru.nbytes != int64(len("key")+len("111")) {
 		t.Fatalf("excepted %v but got %v", int64(len("key")+len("111")), lru.nbytes)
+	}
+}
+
+func TestCache_RemoveOldest(t *testing.T) {
+	k1, k2, k3 := "key1", "key2", "key3"
+	v1, v2, v3 := "value1", "value2", "v3"
+	lru := New(int64(len(k1+k2+v1+v2)), nil)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
+	if _, ok := lru.Get("key1"); ok || lru.Len() != 2 {
+		t.Fatalf("removeOLdest Failed, expected remove oldest key %v, but not removed", k1)
+	}
+}
+
+func TestCache_OnEvicted(t *testing.T) {
+	keys := make([]string, 0)
+	callback := func(key string, value Value) {
+		keys = append(keys, key)
+	}
+	lru := New(int64(5000), callback)
+	lru.Add("key1", String("wwwwww"))
+	lru.Add("k2", String("wwwwww"))
+	lru.Add("k--3", String("wwwwww"))
+	lru.RemoveOldest()
+	lru.RemoveOldest()
+	lru.RemoveOldest()
+	lru.RemoveOldest()
+	lru.RemoveOldest()
+	expected := []string{"key1", "k2", "k--3"}
+	if !reflect.DeepEqual(expected, keys) {
+		t.Fatalf("Call OnEvicted failed, expect keys equals to %s", expected)
 	}
 }
