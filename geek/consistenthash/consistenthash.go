cf45897b4586d9551ad86ae3f462c5d886b7a512
Makonike <1694585033@qq.com>
2023-01-20 18:55:04 +0800

    feat: consistenthash

---
+++ geek/consistenthash/consistenthash.go
@@ -0,0 +1,58 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+type Hash func(data []byte) uint32
+
+type Map struct {
+	hash     Hash           // hash func
+	replicas int            // 虚拟节点倍数
+	keys     []int          // 哈希环，维护有序
+	hashMap  map[int]string // 虚拟节点与真实节点的映射表（key是虚拟节点hash, value is the name of reality node）
+}
+
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		hash:     fn,
+		replicas: replicas,
+		hashMap:  make(map[int]string),
+	}
+	// default use crc32.ChecksumIEEE as hash func
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
+
+// Add adds some keys to the hash.
+// keys is the name of reality node
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		// 一个真实节点对应多个虚拟节点
+		for i := 0; i < m.replicas; i++ {
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			m.keys = append(m.keys, hash)
+			// 维护虚拟节点与真实节点的映射关系
+			m.hashMap[hash] = key
+		}
+	}
+	sort.Ints(m.keys)
+}
+
+// Get gets the closest item in the hash to the provided key.
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+	hash := int(m.hash([]byte(key)))
+	// 顺时针找到第一个匹配的虚拟节点
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+	// 如果没有找到匹配的虚拟节点，返回哈希环上的第一个节点
+	return m.hashMap[m.keys[idx%len(m.keys)]]
+}
---
+++ geek/consistenthash/consistenthash_test.go
@@ -0,0 +1,33 @@
+package consistenthash
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestMap_Get(t *testing.T) {
+	hash := New(3, func(data []byte) uint32 {
+		i, _ := strconv.Atoi(string(data))
+		return uint32(i)
+	})
+	// add vir-node 06, 16, 26, 04, 14, 24, 02, 12, 22
+	hash.Add("6", "4", "2")
+	testCases := map[string]string{
+		"2":  "2", // 02 - 2
+		"11": "2", // 12 - 2
+		"23": "4", // 24 - 4
+		"27": "2", // 02 - 2
+	}
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+			t.Errorf("hash.Get(%s) expeted %s, but %s", k, v, hash.Get(k))
+		}
+	}
+	hash.Add("8")
+	testCases["27"] = "8" // 28 - 8
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+			t.Errorf("hash.Get(%s) expeted %s, but %s", k, v, hash.Get(k))
+		}
+	}
+}
