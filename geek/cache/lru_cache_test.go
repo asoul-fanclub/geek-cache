28c0ecf015d2ee4715e1c42a10a4c9e69a0daa36
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-03-06 19:57:23 +0800

    test:add tests for lru_cache

--- geek/cache/lru_cache_test.go
+++ geek/cache/lru_cache_test.go
@@ -8,6 +8,7 @@ import (
 	"time"
 )
 
+// 检测并发情况下是否会出现问题
 func TestCache_GetAndAdd(t *testing.T) {
 	var wg sync.WaitGroup
 	cache := NewLRUCache(1000000000)
@@ -33,6 +34,7 @@ func TestCache_GetAndAdd(t *testing.T) {
 	}
 }
 
+// 检测lru算法
 func TestCache_FreeMemory(t *testing.T) {
 	a := assert.New(t)
 	// 测试LRU
@@ -40,18 +42,52 @@ func TestCache_FreeMemory(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		cache.Add(strconv.Itoa(i), &testValue{"123456789"})
 	}
-	// 第一个被淘汰
-	_, f := cache.Get("0")
-	a.False(f)
-	// 读取第二个，并再添加一个缓存
-	v1, _ := cache.Get("1")
-	a.Equal("123456789", v1.(*testValue).b)
+	// key为0的被淘汰
+	_, f0 := cache.Get("0")
+	a.False(f0)
+	// key为1的未被淘汰
+	v1, f1 := cache.Get("1")
+	a.True(f1)
+	a.Equal(v1.(*testValue).b, "123456789")
+	// 添加一个缓存
 	cache.Add("a", &testValue{"123456789"})
-	// 第key为2的缓存被淘汰
+	// key为2的缓存被淘汰
 	_, f2 := cache.Get("2")
 	a.False(f2)
+	// key为3未被淘汰
+	v3, f3 := cache.Get("3")
+	a.True(f3)
+	a.Equal(v3.(*testValue).b, "123456789")
 }
 
+// 检测AddWithExpiration的算法中lru逻辑
+func TestCache_FreeMemory2(t *testing.T) {
+	timeout := time.Now().Add(3 * time.Second)
+	a := assert.New(t)
+	// 测试LRU
+	cache := NewLRUCache(90)
+	for i := 0; i < 10; i++ {
+		cache.AddWithExpiration(strconv.Itoa(i), &testValue{"123456789"}, timeout)
+	}
+	// key为0的被淘汰
+	_, f0 := cache.Get("0")
+	a.False(f0)
+	// key为1的未被淘汰
+	v1, f1 := cache.Get("1")
+	a.True(f1)
+	a.Equal(v1.(*testValue).b, "123456789")
+	// 添加一个缓存
+	cache.AddWithExpiration("a", &testValue{"123456789"}, timeout)
+	// key为2的缓存被淘汰
+	_, f2 := cache.Get("2")
+	a.False(f2)
+	// key为3未被淘汰
+	v3, f3 := cache.Get("3")
+	a.True(f3)
+	a.Equal(v3.(*testValue).b, "123456789")
+}
+
+// 测试超时
 func TestCache_AddWithExpiration(t *testing.T) {
 	a := assert.New(t)
 	cache := NewLRUCache(100)
